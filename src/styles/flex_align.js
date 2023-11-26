0dec2f3a3f605c56563557f0fc37e9084c9088b9
yunshin <2younsin@gmail.com>
2023-11-26 17:15:36 +0900

    feat: design Token 생성

---
+++ src/styles/flex_align.js
@@ -0,0 +1,46 @@
+import { css } from 'styled-components'
+
+const alignCenter = css`
+	display: flex;
+	align-items: center;
+`
+const alignStart = css`
+	display: flex;
+	align-items: start;
+`
+const justifyCenter = css`
+	display: flex;
+	justify-content: center;
+`
+const justifyStart = css`
+	display: flex;
+	justify-content: start;
+`
+
+const justifyBetween = css`
+	display: flex;
+	justify-content: space-between;
+`
+const flexCenter = css`
+	${alignCenter}
+	${justifyCenter}
+`
+const flexStart = css`
+	${alignStart}
+	${justifyStart}
+`
+const directionColumn = css`
+	display: flex;
+	flex-direction: column;
+`
+
+export {
+	alignCenter,
+	alignStart,
+	directionColumn,
+	flexCenter,
+	flexStart,
+	justifyBetween,
+	justifyCenter,
+	justifyStart,
+}
---
+++ src/styles/font_weight.js
@@ -0,0 +1,7 @@
+const fontWeight = {
+	bold: 800,
+	regular: 500,
+	thin: 300,
+}
+
+export default fontWeight
