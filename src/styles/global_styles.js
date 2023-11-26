31a020797da37714421bd93198827bbc3119e8d5
yunshin <2younsin@gmail.com>
2023-11-26 21:05:34 +0900

    feat: GlobalStyle 작성

---
+++ src/styles/global_styles.js
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from 'styled-components'
+import fontWeight from './themes/font_weight'
+const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        font-size: 62.5%;
+        font-weight: ${fontWeight.regular};
+    }
+
+    ul, li {
+        list-style: none;
+    }
+
+    h1 {
+      font-size: ${fontSize.big};
+        font-weight: ${fontWeight.bold};
+    }
+
+
+    h2 {
+      font-size: ${fontSize.big};
+        font-weight: ${fontWeight.bold};
+    }
+
+    h3 {
+      font-size: ${fontSize.large};
+        font-weight: ${fontWeight.bold};
+    }
+
+    h4 {
+        font-size: ${fontSize.medium};
+        font-weight: ${fontWeight.bold};
+    }
+
+    button {
+        border: none;
+    }
+
+    input {
+        outline: none;
+    }
+
+    textarea {
+        font-size:${fontSize.small};
+    }
+
+    p, div, span {
+      font-size:${fontSize.small};
+    }
+`
+
+export default GlobalStyles
