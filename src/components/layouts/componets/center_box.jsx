ac0521a213628f1d621841d8bfef873ea87e515d
snkchan <qkrcodbs122@gmail.com>
2023-11-28 01:15:24 +0900

    design: CenterBox 컴포넌트 작업

--- src/components/layouts/componets/center_box.jsx
+++ src/components/layouts/componets/center_box.jsx
@@ -1,5 +1,22 @@
+import styled from 'styled-components'
+import { flexAlign } from '../../../styles/themes/@index'
 const CenterBox = ({ children }) => {
-	return <div>작업해주세요CenterBox{children}</div>
+	return (
+		<S.Div_Wrapper>
+			<S.Div_AlignCenter>{children}</S.Div_AlignCenter>
+		</S.Div_Wrapper>
+	)
 }
 
 export default CenterBox
+const Div_Wrapper = styled.div`
+	width: 100%;
+	${flexAlign.justifyCenter}
+`
+const Div_AlignCenter = styled.div`
+	max-width: 50%;
+`
+const S = {
+	Div_AlignCenter,
+	Div_Wrapper,
+}
