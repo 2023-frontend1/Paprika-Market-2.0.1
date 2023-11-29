f94e580120b19cc06b8b2d8d8624cf31303e784d
yunshin <2younsin@gmail.com>
2023-11-29 20:04:57 +0900

    feat: 공용컴포넌트 폴더 내부에 barrel 파일 생성

---
+++ src/components/@index.js
@@ -0,0 +1,6 @@
+import Button from './button'
+import ImageBox from './image_box'
+import Layout from './layouts/layout'
+import ProfileBadge from './profile_badge'
+
+export { Button, ImageBox, Layout, ProfileBadge }
