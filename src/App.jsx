29198dba6f407672dace8965df39a17fa0b5b8fe
yunshin <2younsin@gmail.com>
2023-12-02 01:54:09 +0900

    chore: 개발 환경 에서, http api 요청 가로채기 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { RouterProvider } from 'react-router-dom'
+import { worker } from './mocks/browser'
 import Router from './routers/router'
 import GlobalStyles from './styles/global_styles'
 
 function App() {
+	if (process.env.NODE_ENV === 'development') {
+		worker && worker.start()
+	}
 	return (
 		<>
 			<GlobalStyles />
